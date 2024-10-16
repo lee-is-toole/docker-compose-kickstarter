9ae07327ef9943efcabbea0e6e0bc9b87cdc57c3
lee-is-toole <129745481+lee-is-toole@users.noreply.github.com>
2024-10-16 09:13:55 +0700

    Fix auto installing modules (#2)
    * Fix auto installing ps-modules
    
    Credit go to "raqso" at https://github.com/raqso/docker-compose-kickstarter/tree/fix/auto-installing-modules
    
    * Recover PS dir ownership after installing additional modules
    
    ---------
    
    Co-authored-by: Lee <lee@inter-soft.com>

--- prestashop/ps_scripts/init-scripts/install_modules.sh
+++ prestashop/ps_scripts/init-scripts/install_modules.sh
@@ -6,11 +6,21 @@ set -e
 INSTALL_COMMAND="${PS_DIR}/bin/console prestashop:module --no-interaction install"
 MODULES_TO_INSTALL=/ps-modules
 
-for file in $(ls "${MODULES_TO_INSTALL}/*.zip"); do
-  module=$(basename ${file} | tr "-" "\n" | head -n 1);
+get_module_name() {
+  local zip_file="$1"
+  local module_name=$(unzip -l "$zip_file" | awk 'NR==4{print $4}' | xargs basename)
+
+  echo "$module_name"
+}
+
+for file in $(ls "${MODULES_TO_INSTALL}"/*.zip); do
+  module=$(get_module_name $file)
   echo "--> installing ${module} from ${file}...";
   rm -rf "${MODULE_DIR}/${module:-something-at-least}"
   unzip -qq ${file} -d ${MODULE_DIR}
   php $INSTALL_COMMAND ${module}
 done;
 
+# If there is module to install after init PS, the files under `var/log` or `var/cache` will be under root
+echo "--> Recover the PS dir ownership"
+chown -R www-data:www-data ${PS_DIR}
